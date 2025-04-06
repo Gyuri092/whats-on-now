b28e2649ce3f9ae7a28fdf95a0db9a889257ee00
Gyuri092 <ggr0927@gmail.com>
2025-04-06 16:13:09 +0900

    init: next.js 프로젝트 세팅(2)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
